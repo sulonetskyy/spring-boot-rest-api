5e329b7b1d9f831c5223a953e560a4433b124aca
sulonetskyy <sergiy.ulonetskyy@gmail.com>
2022-01-13 23:03:57 +0200

    add RequestParams and PathVariables

--- src/main/java/com/example/ui/controller/UserController.java
+++ src/main/java/com/example/ui/controller/UserController.java
@@ -7,8 +7,15 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String getUser() {
-        return "get user was called";
+    public String getUsers(@RequestParam(value="page", defaultValue = "1") int page,
+                           @RequestParam(value="limit", defaultValue = "10") int limit,
+                           @RequestParam(value="sort", defaultValue = "desc", required = false) String sort) {
+        return "get user was page: " + page + ", limit " + limit + ", sort " + sort;
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/{userId}")
+    public String getUser(@PathVariable String userId) {
+        return "get user with id [" + userId + "] was called";
     }
 
     @RequestMapping(method = RequestMethod.POST)
